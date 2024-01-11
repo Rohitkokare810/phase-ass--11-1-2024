68086e05a0cb139e6030db8a6c7c760fe6fe1503
RohitKokare810 <Rohitkokare810@gmail.com>
2024-01-11 15:57:29 +0530

    final

---
+++ ConsoleApp2/ConsoleApp2/Movies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Movies
+    {
+        public int MId { get; set; }
+        public string MTitle { get; set; }
+        public DateTime RDate { get; set; }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+List<Movies> movies = new List<Movies>()
+{
+    new Movies() { MId=1,MTitle="KGF",RDate=new DateTime(day:12,month:11,year:2020)},
+    new Movies() { MId=2,MTitle="KGF2",RDate=new DateTime(day:12,month:11,year:2021)},
+    new Movies() { MId=3,MTitle="KGF3",RDate=new DateTime(day:12,month:11,year:2025)},
+    new Movies() { MId=4,MTitle="SALAAR",RDate=new DateTime(day:12,month:11,year:2023)},
+};
+
+string title;
+Console.WriteLine("Enter title");
+title = Console.ReadLine();
+
+foreach (Movies mov in movies)
+{
+    if (title == mov.MTitle)
+    {
+        Console.WriteLine(mov.MId);
+        Console.WriteLine(mov.MTitle);
+        Console.WriteLine(mov.RDate);
+    }
+
+}
+
